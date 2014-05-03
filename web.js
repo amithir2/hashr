0532fc290a01e955400c92ccb962b26554940720
Chamila Amithirigala <amithir2@illinois.edu>
2014-05-03 13:01:10 -0500

    stringHandler saved in database

--- web.js
+++ web.js
@@ -5,99 +5,6 @@ var fs = require('fs');
 var https = require('https');
 var hbs = require('handlebars');
 var qs = require('querystring');
-var app = express();
-app.use(cookieParser());
-app.use(require("connect").session({ secret: 'keyboard cat'}))
-var mongoose = require('mongoose');
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function callback() {
-
-});
-var hashSchema = new mongoose.Schema({
-	name: Number
-});
-
-var Hash = mongoose.model('Hash', hashSchema);
-mongoose.connect('mongodb://localhost/test');
-
-
-app.get('/', function(req, res){
-
-	var hash = new Hash({
-		name: req.query.name
-	});
-	var count;
-	Hash.count( function(err, size) {
-		if (err) return console.error(err);
-		// if a hashed string was provided, save to db, otherwise ignore
-		if( req.query.name !== undefined && size == 0 ) {
-			hash.save(function(err, hash) {
-				if(err) return console.error(err);
-			});
-		}
-		fs.readFile('./index.hbs', function(err, data){
-			if(err) throw err;
-			var template = hbs.compile(data.toString());
-			Hash.findOne( {}, function(err, obj) {
-				if (err) return console.error(err);
-				hash = obj;
-				var sh;
-				if( hash !== null ){
-					sh = new StringHandler(hash.name);
-				}
-				res.send(template(sh));
-			});
-		});
-	});
-});
-
-app.get('/hashing', function(req, res){
-	fs.readFile('./hashing.hbs', function(err, data){
-		if(err) throw err;
-		var template = hbs.compile(data.toString());
-		res.send(template());
-	});
-});
-
-app.get('/results', function(req, res){
-	fs.readFile('./results.hbs', function(err, data){
-		if(err) throw err;
-		var hash;
-		Hash.findOne( {}, function(err, obj) {
-			if (err) return console.error(err);
-			hash = obj;
-		});
-		var template = hbs.compile(data.toString());
-		res.send(template(hash));
-	});
-	// clear Hash from database
-	// This actually clears the database before we send hash 
-	// to ./results.hbs so that is bad
-	Hash.count( function(err,count) {
-		if (err) return console.error(err);
-		while ( count > 0 )
-		{
-			Hash.findOneAndRemove( {}, function(err) {
-				if (err) return console.error(err);
-			});
-			count = count - 1;
-		}
-	});
-});
-
-app.get('/admin', function(req, res){
-	fs.readFile('./admin.hbs', function(err, data){
-		if(err) throw err;
-		var template = hbs.compile(data.toString());
-		res.send(template());
-		
-	});
-});
-
-var port = Number(process.env.PORT || 5000 );
-	app.listen(port, function() {
-});
 
 /* StringHandler Object:
  * Usage:
@@ -171,3 +78,101 @@ StringHandler.prototype.validate = function (string) {
 StringHandler.prototype.completed = function (string) {
 	this.done[string] = true;
 };
+
+
+var app = express();
+app.use(cookieParser());
+app.use(require("connect").session({ secret: 'keyboard cat'}))
+var mongoose = require('mongoose');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback() {
+
+});
+var hashSchema = new mongoose.Schema({
+	name: Number,
+	stringHandler: mongoose.Schema.Types.Mixed
+});
+
+var Hash = mongoose.model('Hash', hashSchema);
+mongoose.connect('mongodb://localhost/test');
+
+
+app.get('/', function(req, res){
+
+	var hash = new Hash({
+		name: req.query.name,
+		stringHandler: new StringHandler(req.query.name)
+	});
+	var count;
+	Hash.count( function(err, size) {
+		if (err) return console.error(err);
+		// if a hashed string was provided, save to db, otherwise ignore
+		if( req.query.name !== undefined && size == 0 ) {
+			hash.save(function(err, hash) {
+				if(err) return console.error(err);
+			});
+		}
+		fs.readFile('./index.hbs', function(err, data){
+			if(err) throw err;
+			var template = hbs.compile(data.toString());
+			Hash.findOne( {}, function(err, obj) {
+				if (err) return console.error(err);
+				hash = obj;
+				var sh;
+				if( hash !== null ){
+					sh = hash.stringHandler;
+				}
+				console.log(obj);
+				res.send(template(sh));
+			});
+		});
+	});
+});
+
+app.get('/hashing', function(req, res){
+	fs.readFile('./hashing.hbs', function(err, data){
+		if(err) throw err;
+		var template = hbs.compile(data.toString());
+		res.send(template());
+	});
+});
+
+app.get('/results', function(req, res){
+	fs.readFile('./results.hbs', function(err, data){
+		if(err) throw err;
+		var hash;
+		Hash.findOne( {}, function(err, obj) {
+			if (err) return console.error(err);
+			hash = obj;
+		});
+		var template = hbs.compile(data.toString());
+		res.send(template(hash));
+	});
+	// clear Hash from database
+	// This actually clears the database before we send hash 
+	// to ./results.hbs so that is bad
+	Hash.count( function(err,count) {
+		if (err) return console.error(err);
+		while ( count > 0 )
+		{
+			Hash.findOneAndRemove( {}, function(err) {
+				if (err) return console.error(err);
+			});
+			count = count - 1;
+		}
+	});
+});
+
+app.get('/admin', function(req, res){
+	fs.readFile('./admin.hbs', function(err, data){
+		if(err) throw err;
+		var template = hbs.compile(data.toString());
+		res.send(template());
+		
+	});
+});
+
+var port = Number(process.env.PORT || 5000 );
+	app.listen(port, function() {
+});
